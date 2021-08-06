820450d9952555c7c47a5a11399bf9cbe1f3f4be
Nora C <nocorser@gmail.com>
2021-08-06 13:00:41 -0500

    get action by id

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -19,7 +19,22 @@ router.get('/', (req, res) => {
 })
 
 //* GET action - return action as response body
-
+router.get('/:id', (req, res) => {
+    Actions.get(req.params.id)
+        .then(action => {
+           if (!action) {
+                res.status(404).json({
+                    message: "There is no action with that ID.",
+                })
+           } res.json(action)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The action information could not be retrieved.",
+                err: err.message
+            })
+        })
+})
 
 //* POST new action - return new action
 
