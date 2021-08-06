e69e1cf377c04d84e668cd92066284357c8e9796
Nora C <nocorser@gmail.com>
2021-08-06 12:59:20 -0500

    get actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -2,25 +2,32 @@
 const express = require('express');
 const router = express.Router();
 
-// const Action = require('./actions-model');
-// const Project = require('../projects/projects-model');
-
-// const {
-
-// } = require('./actions-middleware');
+const Actions = require('./actions-model');
 
 // endpoints
+//* GET action(s) - return array of actions
+router.get('/', (req, res) => {
+    Actions.get()
+        .then(items => {
+            res.json(items)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The action information could not be retrieved."
+            })
+        })
+})
+
+//* GET action - return action as response body
 
 
+//* POST new action - return new action
 
-// handle errors
-router.use((err, req, res) => {
-    res.status(err.status || 500).json({
-      customMessage: 'Wow, you found an error! Neat!',
-      message: err.message,
-      stack: err.stack
-    })
-  })
+
+//* PUT update action
+
+
+//* DELETE action
   
-  // do not forget to export the router
-  module.exports = router;
+// do not forget to export the router
+module.exports = router;
